1e13a2c7dbac1e2771f754424f87764da9b7a3db
Luis Martinez <luis_mmartinez@live.com>
2021-07-10 14:41:59 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,6 @@ import GifListContainer from '../containers/GifListContainer'
 import NavBar from './NavBar'
 
 // the App component should render out the GifListContainer component 
-// https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=76ieZZ1srp8NC8B06losXXREGIQK6iaF&rating=g
-
-
 
 const App = () => {
   return (
